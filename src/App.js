1ab743a63d44f29bc5bfb4d3397b8d344b4eeb48
David Bronne <davidbronne@gmail.com>
2021-05-22 14:53:31 +0200

    props,map,list

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, {Component} from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component {
+  
+  state= {
+    ninjas:[
+      {name:'Eliot', age:10, belt:'black', id:1},
+      {name:'Cuicui', age:20, belt:'blue', id:2},
+      {name:'Coucou', age:30, belt:'yellow', id:3}
+    ]
+  }
+
   render() {
 
     return (
       <div className="App">
         <h1>My first react app</h1>
         <p>Welcome</p>
-        <Ninjas/>
+        <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
     
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 
 class Ninjas extends Component {
+    
+    
     render() {
+        const {name, age, belt} = this.props;
+
+        const {ninjas} = this.props;
+        console.log(ninjas);
+
+        const ninjasList = ninjas.map( (ninja) => {
+           return(
+
+               <div className="ninja-list" key={ninja.id}>
+
+                <div>Name: {ninja.name}</div>
+               <div>Age: {ninja.age}</div>
+               <div>Belt: {ninja.belt}</div>
+            </div>
+               )
+
+        }
+        )
+
+        
+        
+        
         return (
             <div className="ninja">
-                <div>Name:Eliot</div>
-                <div>Age:10</div>
-                <div>Belt:Black</div>
+
+                {ninjasList}
             </div>
         )
     }
